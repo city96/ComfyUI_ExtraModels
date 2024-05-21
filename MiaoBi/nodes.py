a18ff0a896034464ad8306f8e2b79632f4c9e92a
zmwv823 <913720128@qq.com>
2024-05-21 15:14:33 +0800

    Original MiaoBi diffusers model implementation.

--- MiaoBi/nodes.py
+++ MiaoBi/nodes.py
@@ -1,5 +1,7 @@
+import os
 import folder_paths
 from . import sd
+from . import diffusers_load
     
 class MiaoBi_CLIP:
     @classmethod
@@ -8,15 +10,43 @@ class MiaoBi_CLIP:
                              }}
     RETURN_TYPES = ("CLIP",)
     FUNCTION = "MiaoBi_CLIPLoader"
-    CATEGORY = "ExtraModels//MiaoBi"
+    CATEGORY = "ExtraModels/MiaoBi"
     TITLE = "MiaoBi_CLIP"
 
     def MiaoBi_CLIPLoader(self, clip_name):
         clip_type = sd.CLIPType.STABLE_DIFFUSION
         clip_path = folder_paths.get_full_path("clip", clip_name)
-        clip = sd.load_clip(ckpt_paths=[clip_path], clip_type=clip_type)
+        clip = sd.load_clip(ckpt_paths=[clip_path], embedding_directory=folder_paths.get_folder_paths("embeddings"), clip_type=clip_type)
         return (clip,)
         
+class MiaoBi_Diffusers:
+    @classmethod
+    def INPUT_TYPES(cls):
+        paths = []
+        for search_path in folder_paths.get_folder_paths("diffusers"):
+            if os.path.exists(search_path):
+                for root, subdir, files in os.walk(search_path, followlinks=True):
+                    if "model_index.json" in files:
+                        paths.append(os.path.relpath(root, start=search_path))
+
+        return {"required": {"model_path": (paths,), }}
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
+    FUNCTION = "MiaoBi_Diffusers"
+    CATEGORY = "ExtraModels/MiaoBi"
+    TITLE = "MiaoBi_Diffusers"
+
+    def MiaoBi_Diffusers(self, model_path, output_vae=True, output_clip=True):
+        for search_path in folder_paths.get_folder_paths("diffusers"):
+            if os.path.exists(search_path):
+                path = os.path.join(search_path, model_path)
+                # 拼接os.walk索引的前置路径和选择的文件夹名字，适用于本地。
+                if os.path.exists(path):
+                    model_path = path
+                    break
+
+        return diffusers_load.load_diffusers(model_path, output_vae=output_vae, output_clip=output_clip, embedding_directory=folder_paths.get_folder_paths("embeddings"))
+
 NODE_CLASS_MAPPINGS = {
 	"MiaoBi_CLIP"  : MiaoBi_CLIP,
+    "MiaoBi_Diffusers" :MiaoBi_Diffusers,
 }
